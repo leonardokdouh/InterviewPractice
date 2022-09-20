7fd3c804d0a1fa78053e233717e11c5e0aede573
leonardokdouh <leonardokdouh@gmail.com>
2022-09-20 10:52:04 -0300

    some codes

--- src/main/java/regex/PatternSyntaxChecker.java
+++ src/main/java/regex/PatternSyntaxChecker.java
@@ -1,3 +1,5 @@
+package regex;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,18 +12,17 @@ public class PatternSyntaxChecker {
         int testCases = Integer.parseInt(in.nextLine());
 
         while(testCases>0){
-            String pattern = in.nextLine();
+            String regex = in.nextLine();
         try{
-            Pattern.compile(pattern);
+            Pattern.compile(regex);
             System.out.println("Valid");
+            String word = in.nextLine();
+            System.out.println(word.matches(regex));
         }
             catch (PatternSyntaxException e){
                 System.out.println("Invalid");
             }
-
         testCases--;
         }
-
-
     }
 }
